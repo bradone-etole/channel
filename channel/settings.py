4a90e2de9ebd470577a11817484527762373966e
bradone-etole <omwitsabradone@gmail.com>
2022-04-07 09:32:57 +0300

    update 4.4

--- channel/settings.py
+++ channel/settings.py
@@ -142,4 +142,3 @@ MEDIA="/media/"
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
